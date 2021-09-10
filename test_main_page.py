93e212ccd173be58db3fc1d9b1960ce5fe0c63e8
yarikQA <ilienko030203@gmail.com>
2021-09-11 05:39:28 +0700

    just a little fixing

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,6 @@ from .pages.basket_page import BasketPage
 import pytest
 
 
-
 class TestLoginFromMainPage:
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
